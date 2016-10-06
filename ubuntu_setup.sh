4504a25852355db1f3c9620cae9c83c2b46c74d7
root <root@ip-172-31-38-46.eu-west-1.compute.internal>
2016-10-06 16:51:53 +0000

    Add ubuntu specific setup script

---
+++ ubuntu_setup.sh
@@ -0,0 +1,27 @@
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~                                                                          ~~~"
+echo "~~~   Hello, welcome to the world of Westermann.                             ~~~"
+echo "~~~   This remote script will now install all my cloud computing essentials. ~~~"
+echo "~~~                                                                          ~~~"
+echo "~~~   =====================================================================  ~~~"
+
+sudo apt-get update
+
+# ZSH
+sudo apt-get install zsh
+
+# VIM
+sudo apt-get install vim
+
+# TMUX
+sudo apt-get install tmux
+
+echo "~~~                                                                          ~~~"
+echo "~~~                                                                          ~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+curl https://raw.githubusercontent.com/Westermann/configs/master/setup_configs.sh | sh

c37198c136f6e92c1549db5187d39a9973b67623
Westermann <jonas.westermann@schibsted.com>
2017-04-28 15:03:45 +0000

    Add python3 installs

--- optional_scripts/schibsted_emr.sh
+++ optional_scripts/schibsted_emr.sh
@@ -3,3 +3,8 @@ tar -zxvf Python-3.5.0.tgz
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
 sudo ln -s $(which pip3) /usr/bin/pip3
 sudo yum -y install sqlite-devel
+sudo pip3 install py4j
+sudo pip3 install matplotlib
+sudo pip3 install pandas
+sudo pip3 install psycopg2
+sudo pip3 install seaborn

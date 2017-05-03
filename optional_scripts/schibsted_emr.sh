1212e998fa9d6b885781b62a1654ad94eb983592
jonas-westermann <hadoop@ip-10-0-1-132.eu-west-1.compute.internal>
2017-05-03 14:21:40 +0000

    Add toree scala setup (draft)

--- optional_scripts/schibsted_emr.sh
+++ optional_scripts/schibsted_emr.sh
@@ -1,20 +1,37 @@
-curl -O https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz 
-tar -zxvf Python-3.5.0.tgz
-cd Python-3.5.0
-./configure --enable-loadable-sqlite-extensions && make && sudo make install
-sudo ln -s $(which pip3) /usr/bin/pip3
-sudo yum -y install sqlite-devel
-sudo pip3 install --update pip
-sudo pip3 install py4j
-sudo pip3 install matplotlib
-sudo pip3 install pandas
-sudo pip3 install psycopg2
-sudo pip3 install seaborn
-sudo pip3 install cookiecutter
-sudo pip3 install python-dotenv
+# python and all common dependencies
+# curl -O https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz 
+# tar -zxvf Python-3.5.0.tgz
+# cd Python-3.5.0
+# ./configure --enable-loadable-sqlite-extensions && make && sudo make install
+# sudo ln -s $(which pip3) /usr/bin/pip3
+# sudo yum -y install sqlite-devel
+# sudo pip3 install --update pip
+# sudo pip3 install py4j
+# sudo pip3 install matplotlib
+# sudo pip3 install pandas
+# sudo pip3 install psycopg2
+# sudo pip3 install seaborn
+# sudo pip3 install cookiecutter
+# sudo pip3 install python-dotenv
 
-sudo pip3 install jupyter
-sudo pip3 install jupyter_contrib_nbextensions
-cd $(jupyter --data-dir)/nbextensions
-git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
-jupyter nbextension enable vim_binding/vim_binding
+# all the stuff for jupyter
+# sudo pip3 install jupyter
+# sudo pip3 install jupyter_contrib_nbextensions
+# cd $(jupyter --data-dir)/nbextensions
+# git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+# jupyter nbextension enable vim_binding/vim_binding
+# this is all for apache toree
+curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+sudo yum -y install sbt
+sudo yum -y install gnupg
+mkdir ~/.gnupg
+echo "no-greeting
+personal-digest-preferences SHA512
+cert-digest-algo SHA512
+default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed" >> ~/.gnupg/gpg.conf
+GPG_TTY=`tty`
+cd ~
+git clone git@github.com:apache/incubator-toree.git
+cd incubator-toree
+sed -i 's/2\.0\.0/2.1.0/g' Makefile
+sudo make release

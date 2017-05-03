a7840a5e1afb8f95fffcf99d09097902a11c510e
Westermann <westermann93@googlemail.com>
2017-05-03 09:42:13 +0200

    Update schibsted_emr.sh

--- optional_scripts/schibsted_emr.sh
+++ optional_scripts/schibsted_emr.sh
@@ -1,5 +1,6 @@
 curl -O https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz 
 tar -zxvf Python-3.5.0.tgz
+cd Python-3.5.0
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
 sudo ln -s $(which pip3) /usr/bin/pip3
 sudo yum -y install sqlite-devel

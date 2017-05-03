6de3ac6d8d0e9e3f2eeef4d93afff38edd4fc776
jonas-westermann <jonas.westermann@schibsted.com>
2017-05-03 07:57:19 +0000

    Update emr script

--- optional_scripts/schibsted_emr.sh
+++ optional_scripts/schibsted_emr.sh
@@ -4,8 +4,10 @@ cd Python-3.5.0
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
 sudo ln -s $(which pip3) /usr/bin/pip3
 sudo yum -y install sqlite-devel
+sudo pip3 install --update pip
 sudo pip3 install py4j
 sudo pip3 install matplotlib
 sudo pip3 install pandas
 sudo pip3 install psycopg2
 sudo pip3 install seaborn
+sudo pip3 install jupyter

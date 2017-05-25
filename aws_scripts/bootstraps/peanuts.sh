370636232fa9b498f518798ad9dbe2ef6432807b
Jonas Paul Westermann <westermann93@googlemail.com>
2017-05-25 18:00:24 +0000

    Add peanuts ds challenge script

---
+++ aws_scripts/bootstraps/peanuts.sh
@@ -0,0 +1,6 @@
+curl https://raw.githubusercontent.com/Westermann/configs/master/system_scripts/yum_setup.sh | sh
+git clone https://github.com/Icedgarr/Just-Peanuts/
+cd Just-Peanuts
+curl https://raw.githubusercontent.com/Westermann/configs/jupyter/optional_scripts/jupyter.sh | sh
+make create_environment
+make python_requirements

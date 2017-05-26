a252e9c835ffc71e45902c042a125e1845492503
Westermann <westermann93@googlemail.com>
2017-05-26 12:35:12 +0200

    Update peanuts.sh

--- aws_scripts/bootstraps/peanuts.sh
+++ aws_scripts/bootstraps/peanuts.sh
@@ -1,8 +1,8 @@
 curl https://raw.githubusercontent.com/Westermann/configs/master/system_scripts/yum_setup.sh | sh
 git clone https://github.com/Icedgarr/Just-Peanuts/
 cd Just-Peanuts
-curl https://raw.githubusercontent.com/Westermann/configs/jupyter/optional_scripts/jupyter.sh | sh
 make create_environment
 source venv/bin/activate
 pip install --upgrade pip
 make python_requirements
+curl https://raw.githubusercontent.com/Westermann/configs/jupyter/optional_scripts/jupyter.sh | sh

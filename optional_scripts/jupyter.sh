59f94bc8c765de70a159757c3a5a186e5ca25ca0
Westermann <westermann93@googlemail.com>
2017-07-24 13:10:20 +0200

    Update jupyter.sh

--- optional_scripts/jupyter.sh
+++ optional_scripts/jupyter.sh
@@ -1,4 +1,4 @@
-pip install jupyterthemes
+pip3 install jupyterthemes
 mkdir -p $(jupyter --data-dir)/nbextensions
 cd $(jupyter --data-dir)/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
